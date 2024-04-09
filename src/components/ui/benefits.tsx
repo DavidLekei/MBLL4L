2cc93a24b822ecb05f542e26030dcbef2f0fb346
DAVID <david.w.lekei@gmail.com>
2024-04-09 10:26:59 -0500

    added one more benefit

--- src/components/ui/benefits.tsx
+++ src/components/ui/benefits.tsx
@@ -22,6 +22,12 @@ export default function Benefits(props: any){
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
             </svg>
+            <p>Set up automated results emailed to you (daily, weekly, monthly, etc)</p>
+            </div>
+            <div className="flex flex-row">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+            </svg>
             <p>Set user preferences (Light vs Dark mode, Pagination vs Infinite Scroll, etc)</p>
             </div>
             <div className="flex flex-row">
