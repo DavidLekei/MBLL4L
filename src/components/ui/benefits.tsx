a9cf7d168d18ae52415928484892ce41bea57fbb
DAVID <david.w.lekei@gmail.com>
2024-04-09 13:41:54 -0500

    Changed benefits icons

--- src/components/ui/benefits.tsx
+++ src/components/ui/benefits.tsx
@@ -6,41 +6,32 @@ export default function Benefits(props: any){
         <div className="w-full ">
             {props.showHeader ? header : null}
             <br />
-            <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
+            <div className="flex flex-row items-center">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-            </svg>
-            <p>Export to multiple file types (XML, JSON, etc)</p>
+            </svg> */}
+                <img src="export-48.png" className="mr-5"></img>
+                <p>Export to multiple file types (XML, JSON, etc)</p>
             </div>
-            <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-            </svg>
-            <p>Save searches/filters for quick use</p>
+            <div className="flex flex-row items-center mt-5">
+                <img src="save-48.png" className="mr-5"></img>
+                <p>Save searches/filters for quick use</p>
             </div>
-            <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-            </svg>
-            <p>Set up automated results emailed to you (daily, weekly, monthly, etc)</p>
+            <div className="flex flex-row items-center mt-5">
+                <img src="cycle-48.png" className="mr-5"></img>
+                <p>Set up automated results emailed to you (daily, weekly, monthly, etc)</p>
             </div>
-            <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-            </svg>
-            <p>Set user preferences (Light vs Dark mode, Pagination vs Infinite Scroll, etc)</p>
+            <div className="flex flex-row items-center mt-5">
+                <img src="preferences-48.png" className="mr-5"></img>
+                <p>Set user preferences (Light vs Dark mode, Pagination vs Infinite Scroll, etc)</p>
             </div>
-            <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-            </svg>
-            <p>Access to the API and corresponding documentation</p>
+            <div className="flex flex-row items-center mt-5">
+                <img src="code-48.png" className="mr-5"></img>
+                <p>Access to the API and corresponding documentation</p>
             </div>
-            <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-            </svg>
-            <p>More</p>
+            <div className="flex flex-row items-center mt-5">
+                <img src="more-48.png" className="mr-5"></img>
+                <p>More</p>
             </div>
         </div>
     )
